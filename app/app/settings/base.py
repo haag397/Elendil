3d6688e132c2f22bcf3b5f8db0957f06a34f6652
aghili <haag397@gmail.com>
2024-05-14 21:31:09 +0330

    dockerized project

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -5,7 +5,8 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv() 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECRET_KEY = 'django-insecure-no$05%z%lqsn)9v^j!k9e1zda)19k5@plf$vvtrb)!_^&nxh-)'
--- app/app/settings/local.py
+++ app/app/settings/local.py
@@ -2,23 +2,40 @@
 settings for development
 """
 from .base import *
-
-DOCKER_SECRET_FILE = "/run/secrets/database_password"
+# import os
+# DOCKER_SECRET_FILE = "/run/secrets/database_password"
 # * Read the password from the Docker secret file
-with open(DOCKER_SECRET_FILE, "r") as file:
-    DOCKER_SECRET_PASSWORD = file.read().strip()
-
+# with open(DOCKER_SECRET_FILE, "r") as file:
+#     DOCKER_SECRET_PASSWORD = file.read().strip()
+# try:
+#     with open('/run/secrets/database_password') as f:
+#         database_password = f.read().strip()
+# except FileNotFoundError:
+#         database_password = os.getenv('DATABASE_PASSWORD', 'qju9sj3yy1rdr21b0vudbjhfh')  # fallback
+#         print("=====================================")
+#         print("dddddddddddd",database_password)
 ALLOWED_HOSTS = ["0.0.0.0"]
 
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "database",
+#         "USER": "root",
+#         "PASSWORD": database_password,
+#         # "PASSWORD": "mypassword",
+#         "HOST": "database",
+#         # "HOST": "172.20.0.2",
+#         "PORT": 5432,
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "database",
-        "USER": "myuser",
-        "PASSWORD": DOCKER_SECRET_PASSWORD,
-        # "HOST": "database",
-        "HOST": "172.20.0.2",
-        "PORT": 5432,
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'root',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
